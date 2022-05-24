6c42775a9e6691179dbef9b46ac9f8d5815824b4
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-25 00:32:15 +0300

    fix markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <div className="App">
+                Hello, samurai! Let's go!
+                <Technologies/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
+}
+
