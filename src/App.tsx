aac8c98b317038b27a6421a4f636f67812194a9b
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-26 01:20:37 +0300

    add primary markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import './App.css';
-import Header, {HeaderLink} from './Header';
-import Technologies, {TechnologiesScills} from "./Technologies";
-import Footer from "./Footer";
 
-
-const myScills: Array<TechnologiesScills> = [
-    {id: 1, span: "HTML"},
-    {id: 2, span: "CSS"},
-    {id: 3, span: "JS"},
-    {id: 4, span: "React"}
-]
-
-const Navigation: Array<HeaderLink> = [
-    {id: 5, span: 'Home'},
-    {id: 6, span: 'News Feed'},
-    {id: 7, span: 'Messages'}
-]
 
 const App = () => {
     return (
         <div className="App">
-            <Header navLink={Navigation}/>
-            <Technologies scills={myScills}/>
-            <Footer text={'it-kamasutra.com footer'}/>
+            <header>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNPefPjOAekfOYNxP3Y8OyrzP1I2LSCAgAkicVWOWd-J6kFvIHayhIe6RXzDLskh6HUPU&usqp=CAU"
+                    alt="logotype"/>
+            </header>
+            <nav>
+                <ul>
+                    <li>Profile</li>
+                    <li>Messages</li>
+                    <li>News</li>
+                    <li>Music</li>
+                    <li>Settings</li>
+                </ul>
+            </nav>
+            <section>Main content</section>
         </div>
     );
 }
