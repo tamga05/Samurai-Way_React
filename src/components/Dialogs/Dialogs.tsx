8dcd76530b059db5f7f915b5a7cb533191008156
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-06-13 19:11:57 +0300

    add Dialogs markup (lesson 21)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -8,8 +8,32 @@ type DialogsPropsType = {
 
 const Dialogs = (props: DialogsPropsType) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 };
